3162c40f1f48c50152805c5a7a498bbe12c5a6a1
Hong-She Liang <starofrainnight@gmail.com>
2017-11-24 17:00:37 +0800

    Install printto apps

--- vswprinter/settings.py
+++ vswprinter/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'printto.apps.PrinttoConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
