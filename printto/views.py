b34325244f35dd60f2fdc1ae15be1a6c2baa2ef0
Hong-She Liang <starofrainnight@gmail.com>
2017-11-25 20:09:41 +0800

    Replace directly render() by TemplateView sub-class

--- printto/views.py
+++ printto/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
 
-def index(request):
-    return render(request, 'printto/index.html')
+class IndexView(TemplateView):
+    template_name = "printto/index.html"
