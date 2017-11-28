80b1b37f11eed20c127d3e615b3acfe048d76122
Hong-She Liang <starofrainnight@gmail.com>
2017-11-28 19:33:38 +0800

    Removed directly print messages

--- printto/views.py
+++ printto/views.py
@@ -14,11 +14,9 @@ class IndexView(FormView):
         form_class = self.get_form_class()
         form = self.get_form(form_class)
         if form.is_valid():
-            print("Valid form!")
             form.save()
             return self.form_valid(form)
         else:
-            print("Invalid form!")
             return self.form_invalid(form)
 
 
