5e1016b45dc8d30f09a18a834e05c9b2ceedf706
Hong-She Liang <starofrainnight@gmail.com>
2017-11-29 11:31:38 +0800

    Clear old printed documents

--- printto/apps.py
+++ printto/apps.py
@@ -1,6 +1,16 @@
+import shutil
 from django.apps import AppConfig
 from . import APP_NAME
 
 
 class PrinttoConfig(AppConfig):
     name = APP_NAME
+
+    def ready(self):
+        super().ready()
+
+        from .models import UploadedFileModel
+
+        # Clear printed items when start this app
+        UploadedFileModel.objects.all().delete()
+        shutil.rmtree("./upload", ignore_errors=True)
