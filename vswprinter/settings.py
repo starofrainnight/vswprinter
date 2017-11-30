4caa65383e6c67893f1c7142ec1b84bd6dba3795
Hong-She Liang <starofrainnight@gmail.com>
2017-11-30 23:09:20 +0800

    WhiteNoise library must work with STATIC_ROOT setting!
    So you must defined STATIC_ROOT!

--- vswprinter/settings.py
+++ vswprinter/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -71,8 +72,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'vswprinter.wsgi.application'
 
+# WhiteNoise must work with STATIC_ROOT setting!
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    STATIC_ROOT,
 ]
 
 STATICFILES_FINDERS = [
--- vswprinter/settings_production.py
+++ vswprinter/settings_production.py
@@ -14,7 +14,3 @@ from .settings import *
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
