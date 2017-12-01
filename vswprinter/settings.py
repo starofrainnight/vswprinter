cf468e27581c2b4f9d5099ae132a98321db6f606
Hong-She Liang <starofrainnight@gmail.com>
2017-12-01 15:43:28 +0800

    Minify html files for reduce file transfer size

--- vswprinter/settings.py
+++ vswprinter/settings.py
@@ -50,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'htmlmin.middleware.HtmlMinifyMiddleware',
+    'htmlmin.middleware.MarkRequestMiddleware',
 ]
 
 ROOT_URLCONF = 'vswprinter.urls'
